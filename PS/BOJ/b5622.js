294d6d17d101e735e9962ecdc27dba8fec989ece
Jangmin Lee <jang.xmin@gmail.com>
2021-12-10 12:31:09 +0900

    백준 문제풀이 2021-12-10

--- PS/BOJ/b5622.js
+++ PS/BOJ/b5622.js
@@ -1,18 +1,30 @@
 /**
  * boj.kr/5622
- *
- * A-Z: 65-90
- * 1 : 2s
- * 2 : 3s
- * 3 : 4s...
  */
 
 const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath).toString();
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = fs.readFileSync(filePath).toString().trim();
 console.log(input);
 
-for (let i =)
+const alpabets = {};
+let cnt = 0;
+let num = 3;
+let sum = 0;
 
-// console.log('AZ'.charCodeAt(1));
-// console.log(String.fromCharCode());
+// 알파벳별 소요시간 세팅(A ~ Z)
+for (let i = 65; i <= 90; ++i) {
+  if (cnt === 3 && num < 10) {
+    cnt = 0;
+    num++;
+  }
+  alpabets[String.fromCharCode(i)] = num;
+  cnt++;
+}
+
+// input을 더해서 출력
+for (let i = 0; i < input.length; ++i) {
+  sum += alpabets[input[i]];
+}
+
+console.log(sum);
