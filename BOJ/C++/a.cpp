b19b14e8a72e3a3b3fed2a22793ddbce54a25386
Jangmin Lee <jang.xmin@gmail.com>
2022-01-03 18:20:22 +0900

    백준 문제풀이 2022-01-03

--- BOJ/C++/a.cpp
+++ BOJ/C++/a.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+pair<int, int> p;
+int x, y, z;
 int main() {
-  cout << "Hello World" << "\n";
+  p = make_pair(1, 2);
+  int x = get<0>(p);
+	tie(y, z) = p;
+
+  cout << p.first << " : " << p.second << "\n";  // 1 : 2
+  cout << x << "\n";                             // 1
+  cout << y << " " << z << "\n";                 // 1 2
   return 0;
 }
